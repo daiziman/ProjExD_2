2cd7695620ba92742304ffa27ea71ad21331852d
C0A25256 <c0a2525602@edu.teu.ac.jp>
2026-06-16 14:46:43 +0900

    練習３：画面内外判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -13,6 +13,18 @@ DELTA = {
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRect or ばくだんRect
+    戻り値：判定結果タプル（横方向判定結果，縦方向判定結果）
+    画面内:True 画面外:False
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right: # 横方向判定
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom: # 縦方向判定
+        tate = False
+    return yoko, tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -54,8 +66,16 @@ def main():
                 sum_mv[0] += tpl[0]
                 sum_mv[1] += tpl[1]
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
+
         bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
